571e17373bbf843acfba479c576d4160c256dec6
bm-avatar <bmatusiak@gmail.com>
2023-12-20 11:10:32 -0500

    update

--- post-install.js
+++ post-install.js
@@ -2,34 +2,29 @@ const { exec } = require('child_process')
 var path = require("path");
 
 var myDir = process.cwd()
-console.log(myDir);
 var gunDIR = path.dirname(require.resolve("gun"));
 var jobs = [];
 
 jobs.push((next) => {
-    console.log("unBuilding GUN")
     exec('npm run unbuild', { cwd: gunDIR }, (err, output) => {
         next(err)
     })
 })
 
 jobs.push((next) => {
-    console.log("browserify GUN")
-    exec('npx browserify ./src/index.js -o '+myDir+'/index.js', { cwd: gunDIR }, (err, output) => {
-        next(err)
-    })
-})
-
-jobs.push((next) => {
-    console.log("unBuilding SEA")
     exec('npm run unbuildSea', { cwd: gunDIR }, (err, output) => {
         next(err)
     })
 })
 
+jobs.push((next) => {
+    exec('npx browserify ./gun.js -o '+myDir+'/index.js', { cwd: gunDIR }, (err, output) => {
+        next(err)
+    })
+})
+
 
 jobs.push((next) => {
-    console.log("browserify SEA")
     exec('npx browserify ./sea.js -o '+myDir+'/sea.js', { cwd: gunDIR }, (err, output) => {
         next(err)
     })
